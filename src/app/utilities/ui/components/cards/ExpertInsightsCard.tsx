17148b83468d1d0db489da0049083a1494621df8
Victor Taiwo <51122045+Smallz97@users.noreply.github.com>
2025-05-24 15:54:53 +0100

    fixing expert-insight-cards UI errors

--- src/app/utilities/ui/components/cards/ExpertInsightsCard.tsx
+++ src/app/utilities/ui/components/cards/ExpertInsightsCard.tsx
@@ -8,7 +8,7 @@ export default function ExpertInsightCard({
 }: ExpertInsightCardProps) {
     return (
         <div
-            className={`bg-gradient-to-b ${gradient} rounded-xl outline outline-1 outline-offset-[-0.96px] ${outlineColor} pt-[2.13rem] pb-[3.77rem] px-[1.16rem] lg:py-8 flex flex-col gap-[0.84rem] md:gap-[0.88rem] flex-1 lg:justfiy-center`}
+            className={`bg-gradient-to-b ${gradient} rounded-xl outline  border border-${outlineColor} pt-[2.13rem] pb-[3.77rem] px-[1.16rem] lg:py-8 flex flex-col gap-[0.84rem] md:gap-[0.88rem] flex-1 lg:justfiy-center`}
         >
             <div className="text-slate-950 text-xl font-semibold leading-relaxed md:leading-7">{title}</div>
             <div className="text-gray-800 text-lg font-normal leading-normal">{description}</div>
